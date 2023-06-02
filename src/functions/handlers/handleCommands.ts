6b80db1d3718758eb4f5bdc326f12360332381bd
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-02 18:59:08 +0100

    👨‍💻 Added fully functional handleCommands.

--- src/functions/handlers/handleCommands.ts
+++ src/functions/handlers/handleCommands.ts
@@ -1,5 +1,23 @@
-import {BotClient} from "types";
+import {Routes} from "discord.js";
+import {loopFolders} from "../../functions.js";
+import {BotClient, Command} from "../../types.js";
+import {REST} from "@discordjs/rest";
 
-export const handleCommands = (client: BotClient) => {
-	client.handleCommands = async () => {};
+export default (client: BotClient) => {
+	client.handleCommands = async () => {
+		const {commandArray, commands} = client;
+
+		await loopFolders("commands", async (command) => {
+			const typedCommand = command as Command;
+
+			commandArray.push(typedCommand.data);
+			commands.set(typedCommand.data.name, typedCommand);
+		});
+
+		const rest = new REST({version: "9"}).setToken(process.env.discordBotToken as string);
+
+		await rest.put(Routes.applicationCommands(process.env.discordApplicationID as string), {
+			body: process.env.clearCommands ? [] : commandArray
+		});
+	};
 };
