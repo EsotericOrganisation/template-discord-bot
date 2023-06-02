26f52335eeea63873e675651a46a344e9ee60d66
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-02 18:57:45 +0100

    🗑 Added guildCreate & guildDelete DB management.

---
+++ src/events/client/guild/guildCreate.ts
@@ -0,0 +1,14 @@
+import {Event} from "types";
+import guildSettingsSchema from "../../../schemas/guildSettingsSchema.js";
+import mongoose from "mongoose";
+
+export const guildCreate: Event<"guildCreate"> = {
+	async execute(_client, guild) {
+		const guildSettings = new guildSettingsSchema({
+			_id: new mongoose.Types.ObjectId(),
+			id: guild.id
+		});
+
+		await guildSettings.save();
+	}
+};
---
+++ src/events/client/guild/guildDelete.ts
@@ -0,0 +1,6 @@
+import {Event} from "types";
+import guildSettingsSchema from "../../../schemas/guildSettingsSchema.js";
+
+export const guildDelete: Event<"guildDelete"> = {
+	execute: async (_client, guild) => guildSettingsSchema.deleteOne({id: guild.id})
+};
