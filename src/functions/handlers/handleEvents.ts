c45af9346a023cb1ac345bc74116211dd00c4fce
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-02 19:00:37 +0100

    🔔 Added fully functional handleEvents file.

---
+++ src/functions/handlers/handleEvents.ts
@@ -0,0 +1,43 @@
+import {ClientEvents} from "discord.js";
+import {loopFolders} from "../../functions.js";
+import {BotClient, Event, MongoEvent, ProcessEvent} from "../../types.js";
+import mongoose from "mongoose";
+
+const {connection} = mongoose;
+
+export default (client: BotClient) => {
+	client.handleEvents = async () => {
+		await loopFolders("events/client", async (event, filePath) => {
+			const typedEvent = event as Event<keyof ClientEvents>;
+			const eventName = /\w+(?=\.js)/.exec(filePath)?.[0] as keyof ClientEvents;
+
+			if (typedEvent.once) {
+				client.once(eventName, (...args) => typedEvent.execute(client, ...args) as Promise<void>);
+			} else {
+				client.on(eventName, (...args) => typedEvent.execute(client, ...args) as Promise<void>);
+			}
+		});
+
+		await loopFolders("events/process", async (event, filePath) => {
+			const typedEvent = event as ProcessEvent;
+			const eventName = /\w+(?=\.js)/.exec(filePath)?.[0] as string;
+
+			if (typedEvent.once) {
+				process.once(eventName, typedEvent.execute);
+			} else {
+				process.on(eventName, typedEvent.execute);
+			}
+		});
+
+		await loopFolders("events/mongo", async (event, filePath) => {
+			const typedEvent = event as MongoEvent;
+			const eventName = /\w+(?=\.js)/.exec(filePath)?.[0] as string;
+
+			if (typedEvent.once) {
+				connection.once(eventName, typedEvent.execute);
+			} else {
+				connection.on(eventName, typedEvent.execute);
+			}
+		});
+	};
+};
