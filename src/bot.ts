4b7b20aeb13cc4f989311ecfbe3df606544e9719
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-02 18:54:22 +0100

    📁 Added loop folder call & mongo connection.

--- src/bot.ts
+++ src/bot.ts
@@ -2,21 +2,33 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-import {Client, GatewayIntentBits, Partials} from "discord.js";
+import {Client, Collection, GatewayIntentBits, Partials} from "discord.js";
 import {loopFolders} from "./functions.js";
 import {BotClient} from "./types.js";
+import {connect} from "mongoose";
 
-const {discordBotToken} = process.env;
+const {discordBotToken, mongoDatabaseToken} = process.env;
 
-const client: BotClient = new Client({
+const client = new Client({
 	intents: Object.keys(GatewayIntentBits) as unknown as Partials[],
 	partials: Object.keys(Partials) as unknown as Partials[]
-});
+}) as BotClient;
 
-await loopFolders("functions", async (callback) => {
-	console.log(callback);
+await loopFolders("functions", async (callback) => (callback as Function)(client));
 
-	(callback as Function)(client);
-});
+client.handleEvents().catch(console.error);
+
+client.commandArray = [];
+client.commands = new Collection();
+
+client.handleCommands().catch(console.error);
+
+client.buttons = new Collection();
+client.selectMenus = new Collection();
+client.modals = new Collection();
+
+client.handleComponents().catch(console.error);
 
 await client.login(discordBotToken);
+
+await connect(mongoDatabaseToken as string).catch(console.error);
