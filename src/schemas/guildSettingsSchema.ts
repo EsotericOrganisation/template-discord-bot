6c234b252a063ad6a4aa7471880d2c8496ebb1a5
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-03 19:07:47 +0100

    📷 Added YouTube upload checker to schema.

--- src/schemas/guildSettingsSchema.ts
+++ src/schemas/guildSettingsSchema.ts
@@ -2,7 +2,18 @@ import {Schema, model} from "mongoose";
 
 const guildSettingsSchema = new Schema({
 	id: String,
-	prefix: String
+	prefix: String,
+	youtube: {
+		channels: [
+			{
+				youtubeChannelID: String,
+				youtubeChannelProfilePictureURL: String,
+				latestVideoID: String,
+				discordChannelID: String,
+				pingRoleID: String
+			}
+		]
+	}
 });
 
 export default model("Guild Settings", guildSettingsSchema, "Guild Settings");
