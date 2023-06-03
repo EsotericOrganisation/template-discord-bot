02b976878b7cbf1dacbe47fcf4b46fc29d535aa1
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-03 19:05:12 +0100

    🔘 Added fully functional component handler.

--- src/functions/handlers/handleComponents.ts
+++ src/functions/handlers/handleComponents.ts
@@ -1,5 +1,26 @@
-import {BotClient} from "types";
+import {loopFolders} from "../../functions.js";
+import {BotClient, Button, Modal, SelectMenu} from "../../types.js";
 
-export default (client: BotClient) => {
-	client.handleComponents = async () => {};
+export const handleComponents = (client: BotClient) => {
+	client.handleComponents = async () => {
+		const {buttons, selectMenus, modals} = client;
+
+		await loopFolders("components/buttons", (button) => {
+			const typedButton = button as Button;
+
+			buttons.set(typedButton.name, typedButton);
+		});
+
+		await loopFolders("components/selectMenus", (menu) => {
+			const typedMenu = menu as SelectMenu;
+
+			selectMenus.set(typedMenu.name, typedMenu);
+		});
+
+		await loopFolders("components/modals", (modal) => {
+			const typedModal = modal as Modal;
+
+			modals.set(typedModal.name, typedModal);
+		});
+	};
 };
