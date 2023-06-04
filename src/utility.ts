7db6d4a0039672ada9b57720beaea9d9de364bd4
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-04 22:21:38 +0100

    📜 Added comments, URL & image functions & RegExs.

--- src/utility.ts
+++ src/utility.ts
@@ -1,7 +1,139 @@
+import {readdirSync} from "fs";
+
+/**
+ * A function to loop over a folder containing categories, which themselves contain files. The files are passed as parameters to a callback function. Information about the files is logged to the console.
+ * @param {string} path The path to the folder containing the categories. (from the src directory)
+ * @param {(exports: unknown, filePath: string) => void | Promise<void>} callback The callback function to be called on each file.
+ * @returns {Promise<void>}
+ * @example
+ * // ./src/bot.ts:17:0
+ * import {loopFolders} from "./functions.js";
+ *
+ * ...
+ *
+ * // Loops through the client functions folder and calls the functions.
+ * await loopFolders("functions", (callback) => (callback as Function)(client));
+ */
+export const loopFolders = async (
+	path: string,
+	callback: (exports: unknown, filePath: string) => void | Promise<void>
+): Promise<void> => {
+	const categories = readdirSync(`./dist/${path}/`); // The path starts at src (dist) because most use cases of this function will start there anyway.
+
+	for (const category of categories) {
+		const categoryFiles = readdirSync(`./dist/${path}/${category}`).filter((file) => !file.endsWith(".js.map"));
+
+		for (const file of categoryFiles) {
+			const fileExports = await import(`../dist/${path}/${category}/${file}`);
+
+			// If there is only one export, as is in the case of files such as command, button, modal and select menu exports, then only that export is passed into the function.
+			// If it weren't for this check, then an object with only one property would be passed, making the code a bit more cluttered.
+			// Implementing this is easier than using "export default" everywhere as that can get a bit cluttered when using type declarations.
+
+			const exportKeys = Object.keys(fileExports);
+
+			await callback(
+				exportKeys.length === 1 ? fileExports[exportKeys[0]] : fileExports,
+				`./dist/${path}/${category}/${file}`
+			);
+		}
+	}
+};
+
 /**
  * Some nice colours for embeds and what not.
+ * @example
+ * import {Colours} from "../../../utility.js";
+ *
+ * // Create an embed the colour (coloured side bar) of which exactly matches the colour of the embed itself, making it practically invisible.
+ * // This results in a very clean and professional looking embed.
+ * const embed = {
+ * 	title: "Example Embed",
+ * 	color: Colours.Transparent
+ * };
  */
 export enum Colours {
-	Transparent = 0x2f3136,
+	/**
+	 * A colour that exactly matches the colour of embed backgrounds using dark theme.
+	 */
+	Transparent = 0x2b2d31,
+	/**
+	 * A colour that matches the colour of embed backgrounds using light theme.
+	 * Note: Not confirmed whether the colour actually matches, no way I'm going to enable Discord light theme to test.
+	 */
 	TransparentBright = 0xf2f3f5
 }
+
+/**
+ * A regular expression that matches all URLs in a string. (Global and ignore case flags)
+ * @example
+ * import {URLRegExp} from "../../../utility.js";
+ *
+ * const string = "http://foo.co.uk/ Some example text in between https://marketplace.visualstudio.com/items?itemName=chrmarti.regex Some more random text - https://github.com/chrmarti/vscode-regex";
+ *
+ * const urls = URLRegExp.exec(string);
+ * console.log(urls);
+ * => ["http://foo.co.uk/", "https://marketplace.visualstudio.com/items?itemName=chrmarti.regex", "https://github.com/chrmarti/vscode-regex"];
+ */
+export const URLRegExp =
+	/(https?:\/\/)((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(#[-a-z\d_]*)?/gi;
+
+/**
+ * A function to check if a string is a valid URL or not.
+ * @param {string} urlString The string to check.
+ * @returns {boolean} Whether the string is a valid URL.
+ * @example
+ *
+ * isValidURL("styles.css");
+ * => false;
+ *
+ * isValidURL("https://www.youtube.com");
+ * => true;
+ */
+export const isValidURL = (urlString: string): boolean =>
+	/^(https?:\/\/)((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(#[-a-z\d_]*)?$/i.test(
+		urlString
+	);
+
+/**
+ * A list of all (probably most) valid image file extensions.
+ */
+export const ImageExtensions = [
+	"jpg",
+	"jpeg",
+	"png",
+	"gif",
+	"tiff",
+	"tif",
+	"psd",
+	"pdf",
+	"eps",
+	"ai",
+	"indd",
+	"cr2",
+	"crw",
+	"nef",
+	"pef"
+];
+
+/**
+ * A function to check whether a link leads to an image file based off of it's file extension.
+ * @param {string} urlString The link to check whether it is an image or not.
+ * @returns {boolean} Whether the link stores an image file or not.
+ * @example
+ * import {isImageLink} from "../../../utility.js";
+ *
+ * isImageLink("https://www.youtube.com");
+ * => false;
+ *
+ * isImageLink("https://static.wikia.nocookie.net/minecraft_gamepedia/images/d/dd/Slime_JE3_BE2.png/revision/latest?cb=20191230025505");
+ * => true;
+ */
+export const isImageLink = (urlString: string): boolean => {
+	// For loop instead of forEach or reduce because jump target can not cross function boundary.
+	// I.e. -> "return true" in the callback passed as an argument to the forEach function will return true in the scope of that callback, not that of the whole forEach function.
+	// A regex pattern is used here instead of String.prototype.endsWith because there are certain cases where there can be more text after the file extension, as seen in the second example above.
+	for (const extension of ImageExtensions) if (new RegExp(`\\.${extension}($|\\/[^/]+)`).test(urlString)) return true;
+
+	return false;
+};
