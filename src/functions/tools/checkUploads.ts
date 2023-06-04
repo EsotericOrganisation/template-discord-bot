2fed2033ec4de4353c5a582499312f10959110bb
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-04 22:27:36 +0100

    🚢 Changed imports & a type, added comments.

--- src/functions/tools/checkUploads.ts
+++ src/functions/tools/checkUploads.ts
@@ -1,7 +1,8 @@
 import guildSettingsSchema from "../../schemas/guildSettingsSchema.js";
 import {BotClient} from "types";
 import Parser from "rss-parser";
-import {CategoryChannel, ForumChannel, GuildBasedChannel} from "discord.js";
+import {TextChannel} from "discord.js";
+
 const parser = new Parser();
 
 export default (client: BotClient) => {
@@ -9,7 +10,7 @@ export default (client: BotClient) => {
 		const guilds = await guildSettingsSchema.find();
 
 		for (const guild of guilds) {
-			if (guild.youtube?.channels) {
+			if (guild.youtube?.channels?.length) {
 				let index = 0;
 				for (const channel of guild.youtube.channels) {
 					const channelData = await parser.parseURL(
@@ -23,10 +24,11 @@ export default (client: BotClient) => {
 
 						const discordGuild = await client.guilds.fetch(guild.id);
 
-						const discordChannel = (await discordGuild.channels.fetch(channel.discordChannelID as string)) as Exclude<
-							Exclude<GuildBasedChannel, CategoryChannel>,
-							ForumChannel
-						>;
+						// When the owner or an admin adds a channel, the bot would have checked that the channel is not of type CategoryChannel or type ForumChannel.
+						// That means that the assertion that the channel is of type TextChannel is safe.
+						const discordChannel = (await discordGuild.channels.fetch(
+							channel.discordChannelID as string
+						)) as TextChannel;
 
 						const {title, link, author, isoDate} = channelData.items[0];
 
@@ -45,7 +47,10 @@ export default (client: BotClient) => {
 									},
 									footer: {
 										text: client.user?.username as string,
-										icon_url: client.user?.displayAvatarURL()
+										icon_url: client.user?.displayAvatarURL({
+											forceStatic: false,
+											size: 4096
+										})
 									}
 								}
 							]
