2193f56bee93f0ef3d11bf2238c980fd7b0916ba
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-04 22:23:42 +0100

    📔 Added newline character to log statement.

--- src/events/client/bot/ready.ts
+++ src/events/client/bot/ready.ts
@@ -7,7 +7,7 @@ export const ready: Event<"ready"> = {
 	once: true,
 	async execute(client) {
 		console.log(
-			whiteBright(`${bold("[Client]")} Ready! Online and logged in as ${greenBright(client.user?.username)}.`)
+			whiteBright(`${bold("[Client]")} Ready! Online and logged in as ${greenBright(client.user?.username)}.\n`)
 		);
 
 		setInterval(async () => await client.checkUploads(), 60000);
