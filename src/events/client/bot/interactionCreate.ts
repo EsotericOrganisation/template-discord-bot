f9f1deb72774a421ba17ba76c2f67386d287aae0
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-05 19:48:05 +0100

    🔨 Basic `interactionCreate` event - work needed.

--- src/events/client/bot/interactionCreate.ts
+++ src/events/client/bot/interactionCreate.ts
@@ -1,15 +1,17 @@
 import {InteractionType} from "discord.js";
-import {Command, Event} from "types";
+import {AnyContextMenuCommand, AutoCompleteCommand, Command, Event} from "types";
 
+// Very basic interactionCreate file, will be worked on later.
+// TODO: Make the code better & more concise (don't repeat yourself).
+// TODO: Make a general error handling system.
 export const interactionCreate: Event<"interactionCreate"> = {
 	async execute(client, interaction) {
 		if (interaction.isChatInputCommand()) {
 			const {commands} = client;
-			const {commandName} = interaction;
-			const command = commands.get(commandName);
+			const command = commands.get(interaction.commandName);
 
 			if (!command) {
-				return interaction.reply("No Code");
+				return interaction.reply("❌ Command not found.");
 			}
 
 			try {
@@ -18,10 +20,75 @@ export const interactionCreate: Event<"interactionCreate"> = {
 				console.error(error);
 			}
 		} else if (interaction.isButton()) {
+			const {buttons} = client;
+
+			const button = buttons.get(interaction.customId);
+
+			if (!button) {
+				return interaction.reply("❌ Button not found.");
+			}
+
+			try {
+				await button.execute(interaction, client);
+			} catch (error) {
+				console.error(error);
+			}
 		} else if (interaction.isStringSelectMenu()) {
+			const {selectMenus} = client;
+
+			const selectMenu = selectMenus.get(interaction.customId);
+
+			if (!selectMenu) {
+				return interaction.reply("❌ Select menu not found.");
+			}
+
+			try {
+				await selectMenu.execute(interaction, client);
+			} catch (error) {
+				console.error(error);
+			}
 		} else if (interaction.type === InteractionType.ModalSubmit) {
+			const {modals} = client;
+
+			const modal = modals.get(interaction.customId);
+
+			if (!modal) {
+				return interaction.reply("❌ Modal not found.");
+			}
+
+			try {
+				await modal.execute(interaction, client);
+			} catch (error) {
+				console.error(error);
+			}
 		} else if (interaction.isContextMenuCommand()) {
+			const {commands} = client;
+
+			const command = commands.get(interaction.commandName);
+
+			if (!command) {
+				return console.error(`❌ Command not found: ${interaction.commandName}`);
+			}
+
+			try {
+				await (command as AnyContextMenuCommand).execute(interaction, client);
+			} catch (error) {
+				console.error(error);
+			}
 		} else if (interaction.type === InteractionType.ApplicationCommandAutocomplete) {
+			const {commands} = client;
+
+			const command = commands.get(interaction.commandName);
+
+			if (!command) {
+				return console.error(`❌ Command not found: ${interaction.commandName}`);
+			}
+
+			try {
+				await (command as AutoCompleteCommand).autocomplete(interaction, client);
+			} catch (error) {
+				console.error(error);
+			}
 		}
 	}
 };
