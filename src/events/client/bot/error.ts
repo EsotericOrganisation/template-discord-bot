e4eafa922afcce749d056cff13d635968d05f9a8
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-06 23:59:41 +0100

    🔔 Added error event.

---
+++ src/events/client/bot/error.ts
@@ -0,0 +1,12 @@
+import {Event} from "types";
+import chalk from "chalk";
+
+const {bold, redBright} = chalk;
+
+export const debug: Event<"error"> = {
+	async execute(_client, error) {
+		console.log(redBright(`\n${bold("[Error]")} ${error.name}\n`));
+		console.error(error);
+		console.log("\n");
+	}
+};
