55a4756d3cda7e06737cdbae795a66bf35ea3038
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-06 23:59:26 +0100

    🔔 Added debug event.

---
+++ src/events/client/bot/debug.ts
@@ -0,0 +1,10 @@
+import {Event} from "types";
+import chalk from "chalk";
+
+const {yellow, bold} = chalk;
+
+export const debug: Event<"debug"> = {
+	async execute(_client, message) {
+		if (process.env.debug) console.log(yellow(`${bold("[Debug]")} ${message}`));
+	}
+};
