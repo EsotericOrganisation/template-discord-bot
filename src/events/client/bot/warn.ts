57105adc14948562579cef8b5db06dc400f513be
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-07 18:59:32 +0100

    🔔 Refactored even to use `console.warn` and corrected newlines.

--- src/events/client/bot/warn.ts
+++ src/events/client/bot/warn.ts
@@ -5,8 +5,8 @@ const {yellowBright, bold} = chalk;
 
 export const debug: Event<"warn"> = {
 	async execute(_client, message) {
-		console.log(yellowBright(`\n${bold("[Warning]")}\n`));
-		console.error(message);
-		console.log("\n");
+		console.log(yellowBright(`\n${bold("[Warning]")}`));
+		console.warn(message);
+		console.log();
 	}
 };
