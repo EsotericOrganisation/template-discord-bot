7c9eb4a918c540277cd1ce60920c397bc473f2aa
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-07 15:05:26 +0100

    📔 Added `\n` to start of console log statement.

--- src/events/mongo/connection/err.ts
+++ src/events/mongo/connection/err.ts
@@ -3,7 +3,7 @@ import chalk from "chalk";
 export const err = {
 	execute(error: Error) {
 		console.log(
-			chalk.cyan(`${chalk.bold("[Database Status]")} An error occurred with the database connection: \n${error}\n`)
+			chalk.cyan(`${chalk.bold("\n[Database Status]")} An error occurred with the database connection: \n${error}\n`)
 		);
 	}
 };
