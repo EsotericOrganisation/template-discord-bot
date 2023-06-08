46e4661d6e7ca4c0998655a5d7fb1e74eadc5c24
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-08 19:07:18 +0100

    🔔 +`messageDeleteBulk` event - works the same way as `messageDelete` but for many messages.

---
+++ src/events/client/message/messageDeleteBulk.ts
@@ -0,0 +1,7 @@
+import {Event} from "types";
+
+export const messageDeleteBulk: Event<"messageDeleteBulk"> = {
+	async execute(client, messages) {
+		for await (const message of messages) client.emit("messageDelete", message[1]);
+	}
+};
