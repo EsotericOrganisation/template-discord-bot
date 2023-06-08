4f9b5894d8c2f538741e48805f372054d172f6c1
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-08 19:08:41 +0100

    🔢 Added the `messageCreate` event for the counting system.

---
+++ src/events/client/message/messageCreate.ts
@@ -0,0 +1,46 @@
+import guildSettingsSchema from "../../../schemas/guildSettingsSchema.js";
+import {Event} from "types";
+import {evaluate, isComplex, isResultSet} from "mathjs";
+
+export const messageCreate: Event<"messageCreate"> = {
+	async execute(_client, message) {
+		const {guild} = message;
+
+		if (guild) {
+			const guildSettings = await guildSettingsSchema.findOne({id: guild.id});
+
+			if (guildSettings?.counting?.channels.length) {
+				guildSettings.counting.channels.forEach((countingChannel, index) => {
+					if (message.channelId === countingChannel.channelID && !message.author.bot) {
+						const count = countingChannel.count ?? 0;
+
+						let evaluatedExpression;
+
+						try {
+							evaluatedExpression = evaluate(message.content);
+						} catch (_error) {
+							message.delete().catch(console.error);
+						}
+
+						const expressionValue = isComplex(evaluatedExpression)
+							? evaluatedExpression.re
+							: isResultSet(evaluatedExpression)
+							? evaluatedExpression.entries[evaluatedExpression.entries.length - 1]
+							: evaluatedExpression;
+
+						if (
+							message.author.id !== countingChannel.latestCountAuthorID &&
+							expressionValue === count + 1 &&
+							guildSettings.counting
+						) {
+							guildSettings.counting.channels[index].count = count + 1;
+							guildSettings.counting.channels[index].latestCountAuthorID = message.author.id;
+						} else message.delete().catch(console.error);
+					}
+				});
+
+				await guildSettings.save();
+			}
+		}
+	}
+};
