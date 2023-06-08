9e9c6e5a6beb286c971b5d23144cfb101afb20fe
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-08 19:09:37 +0100

    🔢 Added `/count` - a command to check the count of all counting channels in a guild.

---
+++ src/commands/counting/count.ts
@@ -0,0 +1,31 @@
+import {SlashCommandBuilder} from "discord.js";
+import guildSettingsSchema from "../../schemas/guildSettingsSchema.js";
+import {Command} from "types";
+import {Colours, createErrorMessage} from "../../utility.js";
+
+export const count: Command = {
+	data: new SlashCommandBuilder().setName("count").setDescription("🔢 Get the current count!"),
+	usage: ["count"],
+	async execute(interaction) {
+		const {guild} = interaction;
+
+		const guildSettings = await guildSettingsSchema.findOne({id: guild?.id});
+
+		if (!guild || !guildSettings) await interaction.reply(createErrorMessage("You have to be in a guild to do this!"));
+		else if (!guildSettings.counting?.channels.length)
+			await interaction.reply(createErrorMessage("This guild does not have any counting channels set up!"));
+		else {
+			const {channels} = guildSettings.counting;
+
+			await interaction.reply({
+				embeds: [
+					{
+						title: `🔢 Counting Channels`,
+						description: `${channels.map((channel) => `<#${channel.channelID}> - \`${channel.count}\``)}`,
+						color: Colours.Transparent
+					}
+				]
+			});
+		}
+	}
+};
