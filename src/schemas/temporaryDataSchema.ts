8e02e162ade73c446e17fcea1442502404f6b541
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-09 16:23:28 +0100

    📄 Added a schema for temporary data from `Slime Bot V1`.

---
+++ src/schemas/temporaryDataSchema.ts
@@ -0,0 +1,11 @@
+import {Schema, model} from "mongoose";
+
+const temporaryDataSchema = new Schema({
+	_id: Schema.Types.ObjectId,
+	type: String,
+	data: Object,
+	creationDate: Number,
+	lifeSpan: Number
+});
+
+export default model("Temporary Data", temporaryDataSchema, "Temporary Data");
