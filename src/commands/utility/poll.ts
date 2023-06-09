bbdfb4d6ba865a0a09bdc82dfa6b50c04629be51
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-09 16:47:24 +0100

    📊 Improved `poll` command structure and user-friendly-ness.

--- src/commands/utility/poll.ts
+++ src/commands/utility/poll.ts
@@ -1,4 +1,4 @@
-import {SlashCommandBuilder, ChannelType} from "discord.js";
+import {SlashCommandBuilder, ChannelType, TextChannel} from "discord.js";
 import {evaluate} from "mathjs";
 import mongoose from "mongoose";
 import {
@@ -11,7 +11,7 @@ import {
 import temporaryDataSchema from "../../schemas/temporaryDataSchema.js";
 import {Command} from "../../types";
 
-export const command: Command = {
+export const poll: Command = {
 	data: new SlashCommandBuilder()
 		.setName("poll")
 		.setDescription("📊 Create a poll for others to vote on!")
@@ -26,20 +26,20 @@ export const command: Command = {
 				.addStringOption((option) =>
 					option
 						.setName("colour")
-						.setDescription("The colour of the poll embed. Accepts colour codes and the names of colours.")
+						.setDescription("🌈 The colour of the poll embed. Accepts colour codes and the names of colours.")
 				)
 				.addChannelOption((option) =>
 					option
 						.setName("channel")
-						.setDescription("The channel that the poll should be sent in")
+						.setDescription("💬 The channel that the poll should be sent in")
 						.addChannelTypes(ChannelType.GuildText)
 				)
 				.addStringOption((option) =>
 					option
 						.setName("max-options")
-						.setDescription("The maximum number of options that a user can select.")
+						.setDescription("🔢 The maximum number of options that a user can select.")
 						.addChoices(
-							{name: "Unlimited", value: "Unlimited"},
+							{name: "∞ Unlimited", value: "Unlimited"},
 							{name: "1", value: "1"},
 							{name: "2", value: "2"},
 							{name: "3", value: "3"},
@@ -60,70 +60,63 @@ export const command: Command = {
 				.addStringOption((option) =>
 					option.setName("duration").setDescription(
 						// eslint-disable-next-line quotes
-						'The poll duration. E.g. "5m", "5 minutes", "3 hours", "5 days", "1 week".'
+						'⏳ The poll duration. E.g. "5m", "5 minutes", "3 hours", "5 days", "1 week".'
 					)
 				)
 				.addBooleanOption((option) =>
-					option.setName("anonymous").setDescription("Whether the poll should be sent anonymously.")
+					option.setName("anonymous").setDescription("👤 Whether the poll should be sent anonymously.")
 				)
 				.addRoleOption((option) =>
-					option.setName("required-role").setDescription("The role required to vote on the poll.")
+					option.setName("required-role").setDescription("👤 The role required to vote on the poll.")
 				)
-				.addRoleOption((option) => option.setName("ping").setDescription("The role that the bot should ping."))
+				.addRoleOption((option) => option.setName("ping-role").setDescription("👤 The role that the bot should ping."))
 				.addStringOption((option) =>
 					option
 						.setName("thread-name")
-						.setDescription("If a name is provided, a thread will be created with that name.")
+						.setDescription("💬 If a name is provided, a thread will be created with that name.")
 				)
 				.addStringOption((option) =>
 					option
 						.setName("attachments")
-						.setDescription("Send an attachment. Send multiple by separating links with commas.")
+						.setDescription("🎨 Send an attachment. Send multiple by separating links with commas.")
 				)
-				.addStringOption((option) => option.setName("choice-1").setDescription("The first option."))
-				.addStringOption((option) => option.setName("choice-2").setDescription("The second option."))
+				.addStringOption((option) => option.setName("choice-1").setDescription("1️⃣ The first option."))
+				.addStringOption((option) => option.setName("choice-2").setDescription("2️⃣The second option."))
 
-				.addStringOption((option) => option.setName("choice-3").setDescription("The third option."))
-				.addStringOption((option) => option.setName("choice-4").setDescription("The fourth option."))
-				.addStringOption((option) => option.setName("choice-5").setDescription("The fifth option."))
-				.addStringOption((option) => option.setName("choice-6").setDescription("The sixth option."))
-				.addStringOption((option) => option.setName("choice-7").setDescription("The seventh option."))
-				.addStringOption((option) => option.setName("choice-8").setDescription("The eighth option."))
-				.addStringOption((option) => option.setName("choice-9").setDescription("The ninth option."))
-				.addStringOption((option) => option.setName("choice-10").setDescription("The tenth option."))
+				.addStringOption((option) => option.setName("choice-3").setDescription("3️⃣ The third option."))
+				.addStringOption((option) => option.setName("choice-4").setDescription("4️⃣ The fourth option."))
+				.addStringOption((option) => option.setName("choice-5").setDescription("5️⃣ The fifth option."))
+				.addStringOption((option) => option.setName("choice-6").setDescription("6️⃣ The sixth option."))
+				.addStringOption((option) => option.setName("choice-7").setDescription("7️⃣ The seventh option."))
+				.addStringOption((option) => option.setName("choice-8").setDescription("8️⃣ The eighth option."))
+				.addStringOption((option) => option.setName("choice-9").setDescription("9️⃣ The ninth option."))
+				.addStringOption((option) => option.setName("choice-10").setDescription("🔟 The tenth option."))
 		),
 	usage: [
-		"poll create message: poll question description: description of the poll question channel: channel to send the poll message to colour: colour of the embed of the poll message max-options: maximum number of options a user can select duration: the duration of time users can vote on the poll"
+		"poll create message: poll question description: description of the poll question colour: the colour of the poll embed channel: channel to send the poll message to max-options: maximum number of options a user can select duration: the duration of time that server members will be able to vote on the poll anonymous: whether the poll should be sent anonymously required-role: which role should be required for people to vote on the poll ping-role: which role should the be pinged for the poll thread-name: if provided, will create a thread with that name attachments: any images/other file attachments that should be sent along with the poll ...choices: choices that server members can select - up to 10 allowed"
 	],
 	examples: [
-		"poll create message: Is Cereal a Soup? description: Top scientists have been consumed by this question for years. choice-1: Yes choice-2: No",
-		"poll create message: Is Slime Bot the best bot? description: Truly an intriguing conundrum. choice-1: Yes"
+		"poll create message: 🥣 Is Cereal a Soup? description: Top scientists have been consumed by this question for years. choice-1: Yes choice-2: No",
+		"poll create message: 🤖 Is Slime Bot the best bot? description: Truly an intriguing conundrum. choice-1: Yes",
+		"poll create message: 🐈🐕 Are you a cat or a dog person? description: Which animal do you prefer? choice-1: 🐱 Cat choice-2: 🐶 Dog"
 	],
-	/* .addSubcommand((subcommand) =>
-			subcommand
-				.setName("results")
-				.setDescription("Shows the results of a poll")
-				.addStringOption((option) =>
-					option
-						.setName("message")
-						.setDescription("The ID or link of the poll message.")
-						.setRequired(true)
-				)
-		)*/ async execute(interaction, client) {
-		if (interaction.options.getSubcommand() === "create") {
-			let duration: string | null | number = interaction.options.getString("duration");
+	async execute(interaction, client) {
+		const {options} = interaction;
+
+		if (!interaction.channel) return interaction.reply(createErrorMessage("This command can only be used in a guild!"));
+
+		if (options.getSubcommand() === "create") {
+			let duration: string | number | null = options.getString("duration");
 
 			if (duration) {
 				try {
-					duration = evaluate(
-						`${resolveDate(/^in.+/.test(duration.trim()) ? duration.match(/(?<=^in).+/)[0] : duration)}`
-					);
+					duration = evaluate(`${resolveDate(duration)}`);
 				} catch (error) {
 					return interaction.reply(createErrorMessage(`**Invalid duration provided:** ${duration}\n\n> ${error}`));
 				}
 			}
 
-			const channel = interaction.options.getChannel("channel") ?? interaction.channel;
+			const channel = (interaction.options.getChannel("channel") ?? interaction.channel) as TextChannel;
 
 			const permissions = await checkPermissions(
 				["ViewChannel", "SendMessages", "EmbedLinks"],
@@ -141,12 +134,12 @@ export const command: Command = {
 				ephemeral: true
 			});
 
-			const poll = await new PollMessageBuilder().create(interaction, client);
+			const pollMessage = await new PollMessageBuilder().create(interaction, client);
 
-			const embedMessage = await channel.send(poll);
+			const embedMessage = await channel.send(pollMessage);
 
-			for (const emoji of poll.emojis.filter((emoji) => emoji)) {
-				await embedMessage.react(emoji);
+			for (const emoji of pollMessage.emojis) {
+				if (emoji) await embedMessage.react(emoji);
 			}
 
 			if (duration) {
@@ -166,45 +159,12 @@ export const command: Command = {
 					name: `${interaction.options.getString("thread-name")}`,
 					autoArchiveDuration: 60
 				});
-				if (interaction.options.getRole("ping")) {
-					await thread.send(`<@&${interaction.options.getRole("ping").id}>`);
+				if (interaction.options.getRole("ping-role")) {
+					await thread.send(`<@&${interaction.options.getRole("ping-role").id}>`);
 				}
 			}
 
 			await interaction.editReply(createSuccessMessage(`Successfully created and sent the poll in ${channel}.`));
 		}
-	} /* else {
-			const input = interaction.options.getString("message").split("/");
-
-			const message =
-				(await interaction.channel.messages.fetch(input)) ??
-				(await client.channels.fetch(input[5]).messages.fetch(input[6]));
-
-			switch (message.author.id) {
-				case "880368773960437840": // Slime Bot
-					break;
-				case "155149108183695360": // Dyno
-					break;
-
-				case "235148962103951360": // Carl Bot
-					break;
-
-				case "261157845376958464": // Rock Puppy
-					break;
-
-				case "853327905357561948": // Appy
-					break;
-
-				case "437618149505105920": // Easy Poll
-					break;
-
-				case "324631108731928587": // Simple Poll
-					break;
-
-				case "767149423574515712": // Straw Poll
-					break;
-
-				default:
-			}
-		}*/
+	}
 };
