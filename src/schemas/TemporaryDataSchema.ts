587f04d7300417c570c734f8c2c8af1ed26a99bc
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-18 18:20:06 +0100

    📄 Improved `mongoose` schema types.

--- src/schemas/TemporaryDataSchema.ts
+++ src/schemas/TemporaryDataSchema.ts
@@ -2,10 +2,10 @@ import {Schema, model} from "mongoose";
 
 const TemporaryDataSchema = new Schema({
 	_id: Schema.Types.ObjectId,
-	type: String,
-	data: Object,
-	creationDate: Number,
-	lifeSpan: Number,
+	type: {type: String, required: true},
+	data: {type: Object, required: true},
+	creationDate: {type: Number, default: Date.now},
+	lifeSpan: {type: Number, required: true},
 });
 
 export default model("Temporary Data", TemporaryDataSchema, "Temporary Data");
