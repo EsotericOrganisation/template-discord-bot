d62f6ffb1db28bc3cc42a2f540d78239f82489df
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-18 19:34:01 +0100

    🎈 Implemented a `TypeScript` `interface` in the `GuildDataSchema`.

--- src/schemas/GuildDataSchema.ts
+++ src/schemas/GuildDataSchema.ts
@@ -1,54 +1,136 @@
 import {Schema, model} from "mongoose";
 
-const GuildDataSchema = new Schema({
-	id: String,
-	youtube: {
-		channels: [
-			{
-				youtubeChannelID: {type: String, required: true},
-				youtubeChannelProfilePictureURL: String,
-				latestVideoID: String,
-				discordChannelID: String,
-				pingRoleID: String,
-			},
-		],
-	},
-	starboard: {
-		channels: [
-			{
-				channelID: String,
-				pingRoleID: String,
-				emojiID: String,
-				// While the "emoji ID" technically isn't an ID in all cases, it's still more clear to call the variable 'emojiID' rather than 'emojiIDOrName', and simply calling it 'emoji' could cause some confusion.
-				emojiCount: Number,
-				starredMessageIDs: {
-					type: Object,
-					of: String,
+interface IGuildDataSchema {
+	_id: Schema.Types.ObjectId;
+	id: string;
+	settings?: {
+		youtube?: {
+			disabled?: boolean;
+			channels: {
+				youtubeChannelID: string;
+				latestVideoID: string;
+				discordChannelID: string;
+				pingRoleID?: string;
+			}[];
+		};
+		starboard?: {
+			disabled?: boolean;
+			channels: {
+				channelID: string;
+				pingRoleID?: string;
+				emoji?: string;
+				emojiCount?: number;
+				starredMessageIDs?: {[key: string]: string};
+			}[];
+		};
+		counting?: {
+			disabled?: boolean;
+			channels: {
+				channelID: string;
+				count?: number;
+				latestCountAuthorID?: string;
+			}[];
+		};
+		levelling?: {
+			disabled?: boolean;
+			// The minimum and maximum number of XP that a user can get from a message.
+			minMessageXP?: number;
+			maxMessageXP?: number;
+			// The amount of time (in seconds) that a user needs to wait before being able to gain experience from sending messages again.
+			coolDown?: number;
+			// Different XP bonuses that the user can receive from a message.
+			punctuationBonus?: number; // Default: 1
+			characterBonus?: number; // Default: 0.1
+			sentenceBonus?: number; // Default: 2
+			lineBonus?: number; // Default: 3
+			paragraphBonus?: number; // Default: 4
+			wordBonus?: number; // Default: 0.1
+			linkBonus?: number; // Default: 1
+			attachmentBonus?: number; // Default: 3
+			// Minimum and maximum random XP.
+			minRandomXP?: number; // Default: 5
+			maxRandomXP?: number; // Default: 10
+		};
+	};
+	userExperienceData?: {
+		[key: string]: {
+			experience: number;
+			lastMessageTimestamp: string;
+		};
+	};
+}
+
+const GuildDataSchema = new Schema<IGuildDataSchema>({
+	_id: Schema.Types.ObjectId,
+	id: {type: String, required: true},
+	settings: {
+		youtube: {
+			disabled: Boolean,
+			channels: [
+				{
+					youtubeChannelID: {type: String, required: true},
+					latestVideoID: {type: String, required: true},
+					discordChannelID: {type: String, required: true},
+					pingRoleID: String, // There may be no ping role.
 				},
+			],
+		},
+		starboard: {
+			disabled: Boolean,
+			channels: [
+				{
+					channelID: {type: String, required: true},
+					pingRoleID: String, // There may be no ping role.
+					emoji: {type: String}, // Default: "⭐"
+					emojiCount: {type: Number}, // Default: 3
+					starredMessageIDs: {
+						type: Object,
+						of: {type: String, required: true},
+					}, // There may be no starred messages.
+				},
+			],
+		},
+		counting: {
+			disabled: Boolean,
+			channels: [
+				{
+					channelID: {type: String, required: true},
+					count: Number, // Default: 0
+					// There may be no latest count (the counting channel has just been set up).
+					latestCountAuthorID: String,
+				},
+			],
+		},
+		levelling: {
+			disabled: Boolean,
+			// The minimum and maximum number of XP that a user can get from a message.
+			minMessageXP: Number,
+			maxMessageXP: Number,
+			// The amount of time (in seconds) that a user needs to wait before being able to gain experience from sending messages again.
+			coolDown: Number,
+			// Different XP bonuses that the user can receive from a message.
+			punctuationBonus: Number, // Default: 1
+			characterBonus: Number, // Default: 0.1
+			sentenceBonus: Number, // Default: 2
+			lineBonus: Number, // Default: 3
+			paragraphBonus: Number, // Default: 4
+			wordBonus: Number, // Default: 0.1
+			linkBonus: Number, // Default: 1
+			attachmentBonus: Number, // Default: 3
+			// Minimum and maximum random XP.
+			minRandomXP: Number, // Default: 5
+			maxRandomXP: Number, // Default: 10
+		},
+	},
+	userExperienceData: {
+		type: Object,
+		of: {
+			type: {
+				experience: String,
+				lastMessageTimestamp: String,
 			},
-		],
-	},
-	counting: {
-		channels: [{channelID: String, count: Number, latestCountAuthorID: String}],
-	},
-	levels: {
-		// The minimum and maximum number of XP that a user can get from a message.
-		minMessageXP: Number,
-		maxMessageXP: Number,
-		// The amount of time (in seconds) that a user needs to wait before being able to gain experience from sending messages again.
-		coolDown: Number,
-		// Different XP bonuses that the user can receive from a message.
-		punctuationBonus: Number,
-		characterBonus: Number,
-		sentenceBonus: Number,
-		lineBonus: Number,
-		paragraphBonus: Number,
-		wordBonus: Number,
-		linkBonus: Number,
-		attachmentBonus: Number,
-		// Minimum and maximum random XP.
-		minRandomXP: Number,
-		maxRandomXP: Number,
+			required: true,
+		},
 	},
 });
 
