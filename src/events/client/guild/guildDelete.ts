512eaeda17e1024c991ccb83997177cc73e55f75
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-18 20:03:01 +0100

    📄 Added `await`.

--- src/events/client/guild/guildDelete.ts
+++ src/events/client/guild/guildDelete.ts
@@ -2,5 +2,6 @@ import {Event} from "types";
 import GuildDataSchema from "../../../schemas/GuildDataSchema.js";
 
 export const guildDelete: Event<"guildDelete"> = {
-	execute: async (_client, guild) => GuildDataSchema.deleteOne({id: guild.id}),
+	execute: async (_client, guild) =>
+		await GuildDataSchema.deleteOne({id: guild.id}),
 };
