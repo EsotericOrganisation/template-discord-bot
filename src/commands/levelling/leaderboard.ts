6fe658689ec18594f9a95ce60415e3aadd60d32d
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-19 19:12:53 +0100

    🏆 Added a basic `leaderboard` command.

---
+++ src/commands/levelling/leaderboard.ts
@@ -0,0 +1,29 @@
+import {ErrorMessage, LevelLeaderboardMessage} from "../../utility.js";
+import {Command} from "types";
+import GuildDataSchema from "../../schemas/GuildDataSchema.js";
+import {SlashCommandBuilder} from "discord.js";
+
+export const leaderboard: Command = {
+	data: new SlashCommandBuilder()
+		.setName("leaderboard")
+		.setDescription("🏆 View the users with the top level in the server!")
+		.addNumberOption((option) =>
+			option
+				.setName("page")
+				.setDescription("📄 The page of the leaderboard to view.")
+				.setMinValue(1),
+		),
+	async execute(interaction, client) {
+		const guildData = await GuildDataSchema.findOne({id: interaction.guildId});
+
+		if (!guildData) {
+			return interaction.reply(
+				new ErrorMessage("You need to be in a guild to run this command!"),
+			);
+		}
+
+		return interaction.reply(
+			await new LevelLeaderboardMessage().create(interaction),
+		);
+	},
+};
