52705d8f041c387fb81e8dca753c233087126e00
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-25 19:12:58 +0100

    🎫 Added option to reopen the ticket. (WIP)

---
+++ src/components/buttons/tickets/ticketReopen.ts
@@ -0,0 +1,39 @@
+import {
+	CategoryChannel,
+	Guild,
+	PermissionFlagsBits,
+	TextChannel,
+} from "discord.js";
+import {Button} from "types";
+import {SuccessMessage} from "../../../utility.js";
+
+export const ticketReopen: Button = {
+	async execute(interaction) {
+		await interaction.deferUpdate();
+
+		const guild = interaction.guild as Guild;
+		const channel = interaction.channel as TextChannel;
+
+		const ticketCategoryChannelID = (
+			/^\d+/.exec((channel.topic as string).slice(15)) as RegExpExecArray
+		)[0];
+
+		if (ticketCategoryChannelID !== channel.parentId) {
+			await channel.setParent(ticketCategoryChannelID, {
+				lockPermissions: false,
+			});
+
+			const ticketCategoryChannel = (await guild.channels.fetch(
+				ticketCategoryChannelID,
+			)) as CategoryChannel;
+		}
+
+		await interaction.message.delete();
+
+		await channel.send(
+			new SuccessMessage("Successfully re-opened the ticket!"),
+		);
+
+		await channel.setName(channel.name.replace("closed", "ticket"));
+	},
+};
