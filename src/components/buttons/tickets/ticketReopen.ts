93447fc68f2b6f0bed48434640333fecf5ff281e
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-27 20:37:59 +0100

    🌸 Formatted code, removed unused variables, and added space after emoji.

--- src/components/buttons/tickets/ticketReopen.ts
+++ src/components/buttons/tickets/ticketReopen.ts
@@ -1,9 +1,4 @@
-import {
-	CategoryChannel,
-	Guild,
-	PermissionFlagsBits,
-	TextChannel,
-} from "discord.js";
+import {TextChannel} from "discord.js";
 import {Button} from "types";
 import {Emojis, SuccessMessage} from "../../../utility.js";
 
@@ -11,7 +6,6 @@ export const ticketReopen: Button = {
 	async execute(interaction) {
 		await interaction.deferUpdate();
 
-		const guild = interaction.guild as Guild;
 		const channel = interaction.channel as TextChannel;
 
 		const ticketCategoryChannelID = (
@@ -22,16 +16,14 @@ export const ticketReopen: Button = {
 			await channel.setParent(ticketCategoryChannelID, {
 				lockPermissions: false,
 			});
-
-			const ticketCategoryChannel = (await guild.channels.fetch(
-				ticketCategoryChannelID,
-			)) as CategoryChannel;
 		}
 
 		await interaction.message.delete();
 
 		await channel.send(
-			new SuccessMessage(`Successfully re-opened the ticket!\n\n${Emojis.Warning}Note: *renaming the channel may take a while!*`),
+			new SuccessMessage(
+				`Successfully re-opened the ticket!\n\n${Emojis.Warning} Note: *renaming the channel may take a while!*`,
+			),
 		);
 
 		await channel.setName(channel.name.replace("closed", "ticket"));
