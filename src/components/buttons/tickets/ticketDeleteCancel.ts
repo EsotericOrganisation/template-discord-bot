bb615fa6992607829f0032bdd464f31296697a1d
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-27 20:35:38 +0100

    🌸 Formatted code, simplified variables, fixed schema finding generic type issue.

--- src/components/buttons/tickets/ticketDeleteCancel.ts
+++ src/components/buttons/tickets/ticketDeleteCancel.ts
@@ -1,23 +1,29 @@
 import {TextChannel} from "discord.js";
 import {Button, MongooseDocument} from "types";
 import {SuccessMessage} from "../../../utility.js";
-import TemporaryDataSchema, {ITemporaryDataSchema} from "../../../schemas/TemporaryDataSchema.js";
+import TemporaryDataSchema, {
+	ITemporaryDataSchema,
+} from "../../../schemas/TemporaryDataSchema.js";
 
 export const ticketDeleteCancel: Button = {
 	async execute(interaction) {
-  const {message, channel} = interaction;
+		const channel = interaction.channel as TextChannel;
+		const {message} = interaction;
 
 		await interaction.deferUpdate();
 
-  await message.delete()
+		await message.delete();
 
-		const timeoutData = await TemporaryDataSchema.findOne({matchData: {messageID: message.id}}) as MongooseDocument<ITemporaryDataSchema<{timeoutID: Timeout}, {messageID: message.id}>>;
+		const timeoutData = (await TemporaryDataSchema.findOne({
+			matchData: {messageID: message.id},
+		})) as MongooseDocument<
+			ITemporaryDataSchema<{timeoutID: NodeJS.Timeout}, {messageID: string}>
+		>;
 
 		clearTimeout(timeoutData.data.timeoutID);
 
-		await (interaction.channel as TextChannel).send(
+		await channel.send(
 			new SuccessMessage("Successfully cancelled ticket deletion."),
-		);
 		);
 	},
 };
