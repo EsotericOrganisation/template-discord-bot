0ace88aa03e88342ef792c1295705fde5be4a726
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-07 00:42:27 +0100

    Add `DiscordClientEvent` interface

---
+++ src/types/events/DiscordClientEvent.ts
@@ -0,0 +1,8 @@
+import { ClientEvents } from "discord.js";
+import { SlimeBot } from "../../classes/SlimeBot.js";
+import { Event } from "./Event.js";
+
+export interface DiscordClientEvent extends Event {
+    readonly name: keyof ClientEvents
+    execute(bot?: SlimeBot, ...args: ClientEvents[keyof ClientEvents]): Promise<void>;
+}
