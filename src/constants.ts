251910e9e952cebe8de3dbf0481602756ab65ece
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-07 02:08:06 +0100

    Add `constants` file

---
+++ src/constants.ts
@@ -0,0 +1,32 @@
+import tsConfigFile from "../tsconfig.json" with { type: "json" };
+
+export const ascendDirectoryString = "..";
+export const pathSeparator = "/";
+
+export const restVersion = "10";
+
+export const commandPrefix = "/";
+
+export const defaultDistributableFolderPath = "dist";
+export const distributableFolderPath = tsConfigFile.compilerOptions.outDir ?? defaultDistributableFolderPath;
+
+export const commandsFolderName = "commands";
+export const eventsFolderName = "events";
+export const componentsFolderName = "components";
+
+export const clientEventsFolderName = "client";
+export const processEventsFolderName = "process";
+
+export const buttonsFolderName = "buttons";
+export const menusFolderName = "menus";
+export const modalsFolderName = "modals";
+
+export const commandsFolderPath = distributableFolderPath + pathSeparator + commandsFolderName;
+
+export const eventsFolderPath = distributableFolderPath + pathSeparator + eventsFolderName;
+
+export const componentsFolderPath = distributableFolderPath + pathSeparator + componentsFolderName;
+
+export const buttonsFolderPath = componentsFolderPath + pathSeparator + buttonsFolderName;
+export const menusFolderPath = componentsFolderPath + pathSeparator + menusFolderName;
+export const modalsFolderPath = componentsFolderPath + pathSeparator + modalsFolderName;
