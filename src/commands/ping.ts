61b7975a5d991238f13f8eaaa4b4b2c1fa3c78fe
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-07 01:13:03 +0100

    Update `/ping` command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,17 +1,22 @@
 import { inlineCode, SlashCommandBuilder } from "discord.js";
-import { Command } from "../types/Command";
+import { Command } from "../types/Command.js";
 
 export default {
-    data: new SlashCommandBuilder().setName("ping").setDescription("Pings the bot."),
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Pings the bot."),
     async execute(interaction, bot) {
         const message = await interaction.deferReply({
-            fetchReply: true
+            fetchReply: true,
+            ephemeral: true
         });
 
-        const ping = bot.ws.ping + message.createdTimestamp - interaction.createdTimestamp;
+        const messageCreationPing = message.createdTimestamp - interaction.createdTimestamp;
+
+        const totalPing = bot.ws.ping + messageCreationPing;
 
         interaction.editReply({
-            content: `Pong! Latency: ${inlineCode(ping.toString())}`
+            content: `Pong! Latency: ${inlineCode(totalPing.toString())}`
         });
     },
 } as Command
