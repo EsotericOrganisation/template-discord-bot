ebf999b49826f453da8fee6a448ff229c45672ef
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-07 01:13:47 +0100

    Add `interactionCreate` event

---
+++ src/events/client/interactionCreate.ts
@@ -0,0 +1,30 @@
+import { ChatInputCommandInteraction, ButtonInteraction, ModalSubmitInteraction } from "discord.js";
+import { DiscordClientEvent } from "../../types/events/DiscordClientEvent.js";
+
+export default {
+	name: "interactionCreate",
+	async execute(bot, interaction) {
+		if (interaction instanceof ChatInputCommandInteraction) {
+			const { commands } = bot;
+			const { commandName } = interaction;
+
+			const command = commands.get(commandName);
+
+			await command.execute(interaction, bot);
+		} else if (interaction instanceof ButtonInteraction) {
+			const { buttons } = bot;
+			const { customId } = interaction
+
+			const button = buttons.get(customId);
+
+			await button.execute(interaction, bot);
+		} else if (interaction instanceof ModalSubmitInteraction) {
+			const { modals } = bot;
+			const { customId } = interaction;
+
+			const modal = modals.get(customId);
+
+			await modal.execute(interaction, bot);
+		};
+	}
+} as DiscordClientEvent;
