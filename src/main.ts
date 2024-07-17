6ef33c14fa819b0b700ee915404e5253ca1a5ea5
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-17 19:53:24 +0100

    Remove unneeded imports

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
-import configFile from "../config/config.json" with { type: "json" };
-const config = configFile as Configuration;
-
 import { SlimeBotManager } from "./classes/bot/SlimeBotManager.js";
 import { configFilePath } from "./constants.js";
-import { Configuration } from "./types/bot/Configuration.js";
 
 const botManager = new SlimeBotManager();
 await botManager.run(configFilePath);
