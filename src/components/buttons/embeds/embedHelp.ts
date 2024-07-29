2301b6f0069f768480643357b7ae186fc765428a
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-29 18:34:03 +0100

    Remove old project files

--- src/components/buttons/embeds/embedHelp.ts
+++
@@ -1,9 +0,0 @@
-import {Button} from "types";
-
-export const embedHelp: Button = {
-	async execute(interaction) {
-		await interaction.reply({
-			embeds: [{title: "Work in progress!"}],
-		});
-	},
-};
--- src/components/buttons/tickets/ticketCloseCancel.ts
+++
@@ -1,7 +0,0 @@
-import {Button} from "types";
-
-export const ticketCloseCancel: Button = {
-	async execute(interaction) {
-		await interaction.message.delete();
-	},
-};
