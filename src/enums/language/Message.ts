fbb5184d8b93e01d9c841a51333440d7a68aa54f
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-10 00:52:00 +0100

    Organise message order

--- src/enums/language/Message.ts
+++ src/enums/language/Message.ts
@@ -1,29 +1,31 @@
 export type Message =
-    | "ping-command-name"
-    | "ping-command-description"
-    | "ping"
-    | "ping-result"
-
-    | "reload-bot-command-name"
-    | "reload-bot-command-description"
-    | "reloaded-bot-successfully"
-    | "reload-all-command-description"
-    | "reload-all-command-name"
-    | "reloaded-all-bots-successfully"
-
     | "execute-command-name"
     | "execute-command-description"
     | "execute-code"
     | "code-to-execute"
     | "executed-code"
 
-    | "stop-bot-command-name"
-    | "stop-bot-command-description"
-    | "stop-bot-command-permanently-option-name"
-    | "stop-bot-command-permanently-option-description"
-    | "stopping-bot"
+    | "ping-command-name"
+    | "ping-command-description"
+    | "ping"
+    | "ping-result"
+
+    | "reload-all-command-name"
+    | "reload-all-command-description"
+    | "reloaded-all-bots-successfully"
+
+    | "reload-bot-command-name"
+    | "reload-bot-command-description"
+    | "reloaded-bot-successfully"
+
     | "stop-all-command-name"
     | "stop-all-command-description"
     | "stop-all-command-permanently-option-name"
     | "stop-all-command-permanently-option-description"
     | "stopping-all-bots"
+
+    | "stop-bot-command-name"
+    | "stop-bot-command-description"
+    | "stop-bot-command-permanently-option-name"
+    | "stop-bot-command-permanently-option-description"
+    | "stopping-bot"
